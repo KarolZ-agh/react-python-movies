da68ec5c1d16340d18d312a009bcdb2e9426da90
Student <student@d17-327e-pc-13.ipa.ii.agh.edu.pl>
2025-01-25 12:15:51 +0100

    front interacts with backend

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
@@ -8,9 +8,38 @@ function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
 
-    function handleAddMovie(movie) {
+    useEffect(() => {
+    const fetchMovies = async () => {
+        const response = await fetch(`/movies`);
+        if (response.ok) {
+            const movies = await response.json();
+            setMovies(movies);
+        }
+    };
+    fetchMovies();
+}, []);
+
+    async function handleAddMovie(movie) {
+      const response = await fetch('/movies', {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (response.ok) {
         setMovies([...movies, movie]);
         setAddingMovie(false);
+      }
+    }
+
+    async function handleDeleteMovie(movie) {
+      const response = await fetch('/movies/' + movie.id, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        setMovies(movies.filter(m => m !== movie))
+      }
     }
 
     return (
@@ -19,7 +48,7 @@ function App() {
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                              onDeleteMovie={(movie) => handleDeleteMovie(movie)}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
